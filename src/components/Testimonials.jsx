8f61c3bfe4794763fbefdc780cecc63ca5c953b1
Josh <joshuakibwage23@gmail.com>
2025-03-24 12:53:05 +0300

    Modify testimonial component'

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -15,8 +15,7 @@ const Testimonials = () => {
               <figure className="mt-10">
                 <blockquote className="text-center text-xl/8 font-semibold text-gray-900 sm:text-2xl/9">
                   <p>
-                    “Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo expedita voluptas culpa sapiente alias
-                    molestiae. Numquam corrupti in laborum sed rerum et corporis.”
+                  "This app has transformed my cooking! I can now whip up delicious meals with whatever I have on hand."
                   </p>
                 </blockquote>
                 <figcaption className="mt-10">
@@ -25,12 +24,12 @@ const Testimonials = () => {
                     src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                     className="mx-auto size-10 rounded-full"
                   />
-                  <div className="mt-4 flex items-center justify-center space-x-3 text-base">
-                    <div className="font-semibold text-gray-900">Judith Black</div>
+                  <div className="mt-4 flex flex-col space-y-2 items-center justify-center space-x-3 text-base">
+                    <div className="font-semibold text-gray-900">Emily Johnson</div>
                     <svg width={3} height={3} viewBox="0 0 2 2" aria-hidden="true" className="fill-gray-900">
                       <circle r={1} cx={1} cy={1} />
                     </svg>
-                    <div className="text-gray-600">CEO of Workcation</div>
+                    <div className="text-gray-600">Home Chef, Blogger</div>
                   </div>
                 </figcaption>
               </figure>
